470f02c99bccd3d4da01a7728cf6b5ed5df71071
Jane Slooth <sloothes.com@gmail.com>
2019-06-27 23:23:12 +0300

    Update elevator-escape.js

--- loaders/elevator-escape.js
+++ loaders/elevator-escape.js
@@ -8,7 +8,7 @@ var wireMode     = false;
 var stairMode    = true;
 var octreeMode   = false;
 var cabineMode   = true;
-var mirrorMode   = false;
+var mirrorMode   = true;
 var elevatorMode = true;
 
 var ElevatorAssets = {};
