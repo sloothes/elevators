2a57f22a352de288f3d4175f2efef9ba91374235
Jane Slooth <sloothes.com@gmail.com>
2019-06-27 21:43:17 +0300

    Update elevator-escape.js

--- loaders/elevator-escape.js
+++ loaders/elevator-escape.js
@@ -1199,6 +1199,7 @@ function octreeNodeHelper(node){
                     });
                     mesh.material.materials[1] = material;
                     mesh.material.materials[2] = material;
+                    mesh.material.materials[2].emissiveIntensity = 2;
                     material.emissiveMap.needsUpdate = true;
                     material.needsUpdate = true;
                     $(img).remove();
